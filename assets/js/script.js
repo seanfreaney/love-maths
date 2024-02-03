66b3ebf66b96199fd1415c3649f1204fa7d8878d
seanfreaney <>
2024-02-03 08:21:40 +0000

    fix code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = this.getElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if(this.getAttribute("data-type") === "submit") {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
@@ -20,28 +20,29 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 /**
- * The main game loop, called when the script is loaded 
+ * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
 
     // Creates two random numbers between 1 and 25
-    let num1 = math.floor(math.random () *25) + 1;
-    let num2 = math.floor(math.random() * 25) + 1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-    if(gameType === "addition") {
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
 
 function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer () {
+function calculateCorrectAnswer() {
 
 }
 
@@ -49,20 +50,22 @@ function incrementScore() {
 
 }
 
-function incrementWrongAnswer () {
+function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion (operand1, operand2) {
+function displayAdditionQuestion(operand1, operand2) {
+
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
-}
-
-function displaySubtractQuestion () {
 
 }
 
-function displayMultiplyQuestion () {
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
 
 }
